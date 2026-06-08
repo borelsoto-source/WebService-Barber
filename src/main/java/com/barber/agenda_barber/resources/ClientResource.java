f79f0ef2a05e95312ea4417104deb7b5d596e836
Juan Soto <borel.soto@gmail.com>
2026-06-08 16:22:01 -0300

    Dev profile

--- src/main/java/com/barber/agenda_barber/resources/ClientResource.java
+++ src/main/java/com/barber/agenda_barber/resources/ClientResource.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.barber.agenda_barber.entities.Barber;
 import com.barber.agenda_barber.entities.Client;
 import com.barber.agenda_barber.services.ClientService;
 
--- src/main/java/com/barber/agenda_barber/services/BarberServService.java
+++ src/main/java/com/barber/agenda_barber/services/BarberServService.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Service;
 
 import com.barber.agenda_barber.entities.BarberServ;
 import com.barber.agenda_barber.repositories.BarberServRepository;
+import com.barber.agenda_barber.services.exceptions.DatabaseException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class BarberServService {
@@ -26,6 +29,21 @@ public class BarberServService {
 	
 	public BarberServ insert(BarberServ obj) {
 		return repository.save(obj);
+	}
+	
+	public BarberServ update(Long id, BarberServ obj) {
+		try {
+			BarberServ entity = repository.getReferenceById(id);
+			updateData(entity,obj);
+			return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public void updateData(BarberServ entity, BarberServ obj) {
+		entity.setName(obj.getName());
+		entity.setPrice(obj.getPrice());
 	}
 }
 
